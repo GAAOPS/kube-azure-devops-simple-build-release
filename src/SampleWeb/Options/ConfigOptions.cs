09931de03e4a92850b0f3b257321387380ca0018
Ghodrat Ashournia <ghodrat.ashournia@gmail.com>
2022-01-04 19:08:24 +0100

    Initial Commit

---
+++ src/SampleWeb/Options/ConfigOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleWeb.Options
+{
+    public class ConfigOptions
+    {
+        public string Secret { get; set; }
+        public string ApplicationName { get; set; }
+    }
+}
---
+++ src/SampleWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using SampleWeb.Options;
+
+namespace SampleWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(IOptions<ConfigOptions> config)
+        {
+            Configs = config.Value;
+        }
+
+        public ConfigOptions Configs { get; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
